23bec84068ce5d34b9f4adb07c89ee3e8d632768
Samantha Stallings <samantha.stallings1@gmail.com>
2015-01-28 00:12:01 -0800

    adds admin login and register

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,11 +4,14 @@ class AuthController < ApplicationController
 	def authenticate
 		user = User.find_by(email: params[:email])
     sp = ServiceProvider.find_by(email: params[:email])
+    admin = Admin.find_by(email: params[:email])
 
 		if user
 			render json: { auth_token: user.generate_auth_token }
     elsif sp
       render json: { auth_token: sp.generate_auth_token_sp }
+    elsif admin
+      render json: { auth_token: admin.generate_auth_token_admin }
 		else
 			render json: { error: 'Invalid username or password' }, status: :unauthorized
 		end
